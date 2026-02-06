a12f9d119bef8fadde99e9dca146a42cc93b758f
Evgenii Stepanishin <evgenii.stepanishin@agiledrop.com>
2026-02-06 09:38:02 +0100

    Update imageYOffset parameter in news video generation to shift down by 200px, preventing face cut-off in portrait images. This adjustment improves visual presentation in generated videos.

--- lib/auto-generation/news-generator-pt.ts
+++ lib/auto-generation/news-generator-pt.ts
@@ -289,7 +289,7 @@ export async function generateNewsVideo(
       audioTrimStart,
       audioTrimEnd,
       duration: config.template.audio?.duration || 8,
-      imageYOffset: -100, // Shift up to show more of portrait heads
+      imageYOffset: 200, // Shift down 200px to avoid cutting off faces
       jobId,
     });
 
